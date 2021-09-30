f8c781009bdf30e8414209e719e22456d5b086e3
HeatherBerrill <heather.berrill@btinternet.com>
2021-09-30 17:28:54 +0100

    updating users images

--- db/data/development-data/users.js
+++ db/data/development-data/users.js
@@ -3,34 +3,36 @@ module.exports = [
     username: 'tickle122',
     name: 'Tom Tickle',
     avatar_url:
-      'https://www.spiritsurfers.net/monastery/wp-content/uploads/_41500270_mrtickle.jpg'
+      'https://i.pinimg.com/originals/1d/32/56/1d32565de7794a45e1a9d01a044ceba8.png'
   },
   {
     username: 'grumpy19',
     name: 'Paul Grump',
-    avatar_url: 'https://www.tumbit.com/profile-image/4/original/mr-grumpy.jpg'
+    avatar_url:
+      'https://s.abcnews.com/images/Lifestyle/CN_grumpy_dog_3_jt_150728_4x3t_992.jpg'
   },
   {
     username: 'happyamy2016',
     name: 'Amy Happy',
     avatar_url:
-      'https://vignette1.wikia.nocookie.net/mrmen/images/7/7f/Mr_Happy.jpg/revision/latest?cb=20140102171729'
+      'http://st.depositphotos.com/1146092/4811/i/450/depositphotos_48115167-very-funny-dog.jpg'
   },
   {
     username: 'cooljmessy',
     name: 'Peter Messy',
-    avatar_url: 'https://i.imgur.com/WfX0Neu.jpg'
+    avatar_url:
+      'https://images.unsplash.com/photo-1485981133625-f1a03c887f0a?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8c2lsbHklMjBkb2d8ZW58MHx8MHx8&ixlib=rb-1.2.1&w=1000&q=80'
   },
   {
     username: 'weegembump',
     name: 'Gemma Bump',
     avatar_url:
-      'https://www.upandrunning.co.uk/media/catalog/product/cache/1/image/650x/040ec09b1e35df139433887a97daa66f/m/r/mr-bump.jpg'
+      'https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F24%2F2020%2F06%2F05%2FGettyImages-700841103-2000.jpg'
   },
   {
     username: 'jessjelly',
     name: 'Jess Jelly',
     avatar_url:
-      'https://s-media-cache-ak0.pinimg.com/564x/39/62/ec/3962eca164e60cf46f979c1f57d4078b.jpg'
+      'https://www.rd.com/wp-content/uploads/2020/11/GettyImages-889552354-e1606774439626.jpg'
   }
 ];
